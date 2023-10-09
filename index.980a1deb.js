fb470e83b583d1907835dd5e5d1977675bd072a7
NadyaKonzevaya <92791914+NadyaKonzevaya@users.noreply.github.com>
2023-10-09 11:21:44 +0000

    Deploying to gh-pages from @ NadyaKonzevaya/ActiveBox1@c81310d33ebfb777260fa992f636d1c9293a2c24 🚀

---
+++ index.980a1deb.js
@@ -0,0 +1,3 @@
+(()=>{let e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",()=>{let a="true"===e.getAttribute("aria-expanded");e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")})})();//# sourceMappingURL=index.980a1deb.js.map
+
+//# sourceMappingURL=index.980a1deb.js.map
